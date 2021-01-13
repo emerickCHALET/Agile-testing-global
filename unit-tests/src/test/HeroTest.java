a2d07cd9f9436c2fcc76e883ad29dbfc5fb35da9
marinebijon28 <47433408+marinebijon28@users.noreply.github.com>
2021-01-13 15:53:06 +0100

    unit test
    unit test

--- unit-tests/src/test/HeroTest.java
+++ unit-tests/src/test/HeroTest.java
@@ -10,6 +10,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import codingfactory.rpgconsole.hero.Hero;
+import codingfactory.rpgconsole.enemy.Enemy;
 
 public class HeroTest {
 
@@ -38,17 +39,42 @@ public class HeroTest {
 
 	@Test
 	public void testHeroLevelUp() throws Exception {
+		// TODO
+		assertThat(hero, hasProperty("level"));
+		assertThat(hero, hasProperty("level", is(1)));
 		hero.levelUp();
 		assertThat(hero, hasProperty("level"));
 		assertThat(hero, hasProperty("level", is(2)));
-		System.out.println("okd	cc");
-
 	}
 
 	@Test
 	public void testHeroProperties() throws Exception {
 		assertThat(hero, hasProperty("name"));
         assertThat(hero, hasProperty("name", is("Jaina Portvaillant")));
+		assertThat(hero, hasProperty("hp"));
+        assertThat(hero, hasProperty("hp", is(20)));
+		assertThat(hero, hasProperty("atk"));
+        assertThat(hero, hasProperty("atk", is(2)));
+	}
+
+	@Test
+	public void testTakeDamage() throws Exception {
+        assertThat(hero, hasProperty("hp"));
+        assertThat(hero, hasProperty("hp", is(20)));
+        hero.takeDamage(5);
+        assertThat(hero, hasProperty("hp"));
+        assertThat(hero, hasProperty("hp", is(15)));
+	}
+
+	@Test
+	public void testAttack() throws Exception {
+		System.out.println(hero.getLevel());
+        Enemy enemy = new Enemy("Arthas Menethil", 5);
+        assertThat(enemy, hasProperty("hp"));
+        assertThat(enemy, hasProperty("hp", is(15 * 5)));
+        double res = enemy.getHp();
+        hero.attack(enemy);
+      	assertThat((double)enemy.getHp(), is(closeTo(res - 2, res - 4)));
 	}
 
 }
