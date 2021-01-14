2d4f6fab5b5a236ddcf8ab6b80201cfdf19295a7
emerickchalet <emerickchalet@gmail.com>
2021-01-14 17:58:18 +0100

    prenom, nom and number phone done

--- acceptance-tests/src/tesla/acceptance/EvenementsTesla.feature
+++ acceptance-tests/src/tesla/acceptance/EvenementsTesla.feature
@@ -1,1 +1,9 @@
 Feature: Fonctionnalités de ma page Evenements
+  Scenario: remplir de la page evenements
+    Given je suis sur la page event
+    Then je remplis le champ prénom avec "Emerick"
+    And je remplis le champ nom avec "CHALET"
+    And je remplis le champ téléphone avec "0640459520"
+    And je remplis le champ code postal avec "95370"
+    And je click sur "Suivant"
+    Then le message d'erreur apparait
--- acceptance-tests/src/tesla/acceptance/EvenementsTeslaSteps.java
+++ acceptance-tests/src/tesla/acceptance/EvenementsTeslaSteps.java
@@ -28,6 +28,55 @@ public class EvenementsTeslaSteps {
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
+    @Given("^je suis sur la page event$")
+    public void je_suis_sur_la_page_event() throws Throwable {
+
+        driver.get("https://www.tesla.com/fr_FR/events");
+    }
+
+    @Then("^je remplis le champ prénom avec \"([^\"]*)\"$")
+    public void je_remplis_le_champ_prénom_avec(String arg1) throws Throwable {
+
+        driver.findElement(By.xpath("//*[@id=\"edit-firstname-td\"]")).sendKeys(arg1);
+    }
+
+    @Then("^je remplis le champ nom avec \"([^\"]*)\"$")
+    public void je_remplis_le_champ_nom_avec(String arg1) throws Throwable {
+
+        driver.findElement(By.xpath("//*[@id=\"edit-lastname-td\"]")).sendKeys(arg1);
+    }
+
+    @Then("^je remplis le champ téléphone avec \"([^\"]*)\"$")
+    public void je_remplis_le_champ_téléphone_avec(String arg1) throws Throwable {
+
+        driver.findElement(By.xpath("//*[@id=\"edit-phonenumber-td\"]")).sendKeys(arg1);
+    }
+
+    @Then("^je remplis le champ code postal avec \"([^\"]*)\"$")
+    public void je_remplis_le_champ_code_postal_avec(String arg1) throws Throwable {
+
+        driver.findElement(By.xpath("//*[@id=\"edit-phonenumber-td\"]")).sendKeys(arg1);
+    }
+
+    @Then("^je click sur \"([^\"]*)\"$")
+    public void je_click_sur(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^le message d'erreur apparait$")
+    public void le_message_d_erreur_apparait() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^les liens du menus contiennent \"([^\"]*)\" et \"([^\"]*)\"$")
+    public void les_liens_du_menus_contiennent_et(String arg1, String arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+
     @After
     public void afterScenario() {
         driver.quit();
