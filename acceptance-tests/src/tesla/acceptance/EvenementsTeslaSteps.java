267aa503b2b87a97bef8b77f534b33211d9d4734
emerickchalet <emerickchalet@gmail.com>
2021-01-14 17:28:38 +0100

    Update EvenementsTeslaSteps.java

--- acceptance-tests/src/tesla/acceptance/EvenementsTeslaSteps.java
+++ acceptance-tests/src/tesla/acceptance/EvenementsTeslaSteps.java
@@ -1,25 +1,18 @@
 package tesla.acceptance;
 import java.util.concurrent.TimeUnit;
-
 import java.lang.*;
-
 import org.junit.Test;
-
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-
 import static org.junit.Assert.*;
-
 import static org.hamcrest.Matchers.*;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebElement;
 
 public class EvenementsTeslaSteps {
