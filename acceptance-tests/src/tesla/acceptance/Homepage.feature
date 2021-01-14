e0e25e723a627b1d937ddc52a216d5e03e0cc5b1
emerickchalet <emerickchalet@gmail.com>
2021-01-14 20:40:15 +0100

    Merge branch 'Erwan' into Tesla-start

--- acceptance-tests/src/tesla/acceptance/Homepage.feature
+++ acceptance-tests/src/tesla/acceptance/Homepage.feature
@@ -7,39 +7,80 @@ Feature: Fonctionnalités de ma page d'accueil
     Given je suis sur la homepage
     Then la description contient "Tesla accélère la transition mondiale vers une énergie durable en proposant des véhicules électriques, des panneaux solaires et des solutions intégrées d'énergie renouvelable pour les particuliers et les entreprises."
 
-  Scenario: Punchlines de la Homepage
+  Scenario Outline: Punchlines de la Homepage
     Given je suis sur la homepage
-    Then la premiere punchline contient "Model S"
-      And la deuxieme punchline contient "Model 3"
-      And la troisieme punchline contient "Model X"
-      And la quatrieme punchline contient "Model Y"
-      And la cinquieme punchline contient "Systèmes d'énergie solaire et Powerwalls"
-  
+    Then les punchlines contiennent "<punchline>"
+    Examples:
+      | punchline                                |
+      | Model S                                  |
+      | Model 3                                  |
+      | Model X                                  |
+      | Model Y                                  |
+      | Systèmes d'énergie solaire et Powerwalls |
 
 
   Scenario Outline: Liens du menu de la Homepage
     Given je suis sur la homepage
-    Then les liens du menus contiennent "<nom>" et "<link>"
+    Then les liens du menus contiennent "<link>"
     Examples:
-      | nom       | link                                  |
-      | Model S   | https://www.tesla.com/fr_fr/models    |
-      | Model 3   | https://www.tesla.com/fr_fr/model3    |
-      | Model X   | https://www.tesla.com/fr_fr/modelx    |
-      | Model Y   | https://www.tesla.com/fr_fr/modely    |
-      | Powerwall | https://www.tesla.com/fr_fr/powerwall |
-      | Recharger | https://www.tesla.com/fr_fr/charging  |
+      | link                                  |
+      | https://www.tesla.com/fr_FR/models    |
+      | https://www.tesla.com/fr_FR/model3    |
+      | https://www.tesla.com/fr_FR/modelx    |
+      | https://www.tesla.com/fr_FR/modely    |
+      | https://www.tesla.com/fr_fr/powerwall |
+      | https://www.tesla.com/fr_FR/charging  |
 
 
-  Scenario: Liens du burger menu de la Homepage
+  Scenario Outline: Liens du burger menu de la Homepage
     Given je suis sur la homepage
-    Then le premier lien contient burger menu contient "Véhicules disponibles"
-      And le deuxieme lien contient burger menu contient "Véhicules d'occasion"
-      And le troisieme lien contient burger menu contient "Reprise"
-      And le quatrieme lien contient burger menu contient "Cybertruck"
-      And le cinquieme lien contient burger menu contient "Roadster"
-      And le sixieme lien contient burger menu contient "Énergie"
-      And le septieme lien contient burger menu contient "Essais"
-      And le huitieme lien contient burger menu contient "Flottes & Entreprises"
-      And le neuvieme lien contient burger menu contient "Nous trouver"
-      And le dixieme lien contient burger menu contient "Événements"
-      And le onzieme lien contient burger menu contient "Assistance"
+    Then les liens du burger menu contiennent "<link>"
+    Examples:
+      | link                   |
+      | Véhicules disponibles  |
+      | Véhicules d'occasion   |
+      | Reprise                |
+      | Cybertruck             |
+      | Roadster               |
+      | Energie                |
+      | Essais                 |
+      | Véhicules d'occasion   |
+      | Flottes et entreprises |
+      | Nous trouver           |
+      | Evénements             |
+      | Assistance             |
+
+  Scenario:Quand je clique sur le bouton commander, l'url du configurateur de la Model S doit être "https://www.tesla.com/fr_FR/models/design"
+    Given je suis sur le modelS
+    Then je click sur le bouton commander avec lurl "https://www.tesla.com/fr_fr/models/design"
+
+  Scenario: Le prix affiché en LOA est de "à partir de 768 € /mois*"
+    Given je suis sur Model S
+    Then le prix affiché en LOA est de "A partir de 768 € /mois*"
+
+  Scenario: Lorsque je sélectionne le modèle Grande Autonomie Plus, 108€/mois d'économies de carburant estimées
+  et un montant total avec achat au terme du contrat de 94 841€
+    Given je suis sur le model S
+    Then Le prix affiché  est de "A partir de - 108 € /mois"
+    And Quand je click sur "Voir détails"
+    And Le prix du montant total avec achat au terme du contrat de "94 841 €"
+
+  Scenario: Lorsque je sélectionne le modèle "Performance", le prix en LOA est à 969€/mois et 108€/mois d'économies de carburant estimées
+  et un montant total avec achat au terme du contrat de 114 052€.
+    Given je suis sur commander Model S
+    When je click sur Performance "Performance"
+    Then le prix affiché en LOA PERFORMANCE est de "A partir de 969 € /mois*"
+    And Le prix affiché est "A partir de - 108 € /mois"
+    And Quand je click sur le bouton "Voir détails"
+    And Le prix du montant total avec achat au terme du contrat est alors de "114 052 €"
+
+  Scenario: Si j'ajoute l'option pilotage automatique le LOA est "a partir de 857 € / mois*"
+    Given je suis sur commander ModelS
+    When je click sur "Pilotage automatique"
+    Then je click sur l'option "Option ajoutée"
+    Then le prix affiché en LOA est alors de "A partir de 857 € /mois*"
+
+  Scenario: Une fois que j'ai configuré mon modèle S, je souhaiterais connaitre la liste des localisations de vente
+    Given  je suis sur la page  model S
+    Then je configure mon modèle S
+    And je clic donc sur le logo en haut à gauche de la page puis en bas de page sur le lien Localisations j'arrive sur la page dont l'url est : "https://www.tesla.com/fr_FR/findus/list" 
--- acceptance-tests/src/tesla/acceptance/HomepageSteps.java
+++ acceptance-tests/src/tesla/acceptance/HomepageSteps.java
@@ -18,10 +18,9 @@ import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.*;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
-
-import org.openqa.selenium.WebElement;
 
 public class HomepageSteps {
 
@@ -36,107 +35,232 @@ public class HomepageSteps {
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
-    @Given("^je suis sur la homepage$")
-    public void je_suis_sur_la_homepage() throws Throwable {
-        driver.get("https://www.tesla.com/fr_FR/");
+       @Given("^je suis sur la homepage$")
+       public void je_suis_sur_la_homepage() throws Throwable {
+           driver.get("https://www.tesla.com/fr_FR/");
+       }
+
+        @Then("^le titre doit être \"([^\"]*)\"$")
+        public void le_titre_doit_être(String arg1) throws Throwable {
+            assertEquals(driver.getTitle(),arg1);
+        }
+
+        @Then("^la description contient \"([^\"]*)\"$")
+        public void la_description_contient(String arg1) throws Throwable {
+             assertTrue(driver.findElement(By.xpath("//meta[@name='description']")).getAttribute("content").contains(arg1));
+        }
+
+        @Then("^la premiere punchline contient \"([^\"]*)\"$")
+        public void la_premiere_punchline_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//*[contains(text(),'Model S')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^la deuxieme punchline contient \"([^\"]*)\"$")
+        public void la_deuxieme_punchline_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//*[contains(text(),'Model 3')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^la troisieme punchline contient \"([^\"]*)\"$")
+        public void la_troisieme_punchline_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//*[contains(text(),'Model X')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^la quatrieme punchline contient \"([^\"]*)\"$")
+        public void la_quatrieme_punchline_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//*[contains(text(),'Model Y')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^la cinquieme punchline contient \"([^\"]*)\"$")
+        public void la_cinquieme_punchline_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElements(By.cssSelector("h1.tcl-hero-parallax__heading-color-black")).get(5).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^les liens du menus contiennent \"([^\"]*)\" et \"([^\"]*)\"$")
+        public void les_liens_du_menus_contiennent_et(String arg1, String arg2) throws Throwable {
+            assertEquals(driver.findElement(By.xpath("//a[contains(text(), '"+arg1+"' )]")).getAttribute("href"),arg2);
+        }
+
+        @Then("^le premier lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_premier_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Véhicules ')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^le deuxieme lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_deuxieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElements(By.xpath("//a[contains(text(),'Véhicules ')]")).get(3).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^le troisieme lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_troisieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Reprise')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^le quatrieme lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_quatrieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Cybertruck')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^le cinquieme lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_cinquieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Roadster')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^le sixieme lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_sixieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//a[contains(text(), 'Énergie')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^le septieme lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_septieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Essais')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^le huitieme lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_huitieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            String[] tab = arg1.split(" ");
+            WebElement tmp = driver.findElement(By.xpath("//a[contains(text(),'Flottes ')]"));
+            assertTrue(tmp.getAttribute("innerHTML").contains(tab[0]));
+            assertTrue(tmp.getAttribute("innerHTML").contains(tab[2]));
+        }
+
+        @Then("^le neuvieme lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_neuvieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Nous trouver')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^le dixieme lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_dixieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Événements')]")).getAttribute("innerHTML").contains(arg1));
+        }
+
+        @Then("^le onzieme lien contient burger menu contient \"([^\"]*)\"$")
+        public void le_onzieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
+            assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Assistance')]")).getAttribute("innerHTML").contains(arg1));
+
+    @Given("^je suis sur le modelS$")
+    public void je_suis_sur_le_modelS() throws Throwable {
+
+        driver.get("https://www.tesla.com/fr_fr/models");
     }
 
-    @Then("^le titre doit être \"([^\"]*)\"$")
-    public void le_titre_doit_être(String arg1) throws Throwable {
-        assertEquals(driver.getTitle(),arg1);
+    @Then("^je click sur le bouton commander avec lurl \"([^\"]*)\"$")
+    public void je_click_sur_le_bouton_commander_avec_lurl(String arg1) throws Throwable {
+
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"tesla-hero-showcase-1166\"]/div/div[1]/div/div[2]/div/div[6]/a")).getAttribute("href"),arg1);
+        driver.findElement(By.xpath("//*[@id=\"tesla-hero-showcase-1166\"]/div/div[1]/div/div[2]/div/div[6]/a")).click();
+    }
+    @Given("^je suis sur Model S$")
+    public void je_suis_sur_Model_S() throws Throwable {
+        driver.get("https://www.tesla.com/fr_fr/models/design");
     }
 
-    @Then("^la description contient \"([^\"]*)\"$")
-    public void la_description_contient(String arg1) throws Throwable {
-         assertTrue(driver.findElement(By.xpath("//meta[@name='description']")).getAttribute("content").contains(arg1));
+    @Then("^le prix affiché en LOA est de \"([^\"]*)\"$")
+    public void le_prix_affiché_en_LOA_est_de(String arg1) throws Throwable {
+        assertEquals(driver.findElement(By.cssSelector(".finance-item--price.finance-item--price-before-savings")).getText(),arg1);
+
     }
 
-    @Then("^la premiere punchline contient \"([^\"]*)\"$")
-    public void la_premiere_punchline_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//*[contains(text(),'Model S')]")).getAttribute("innerHTML").contains(arg1));
+    @Given("^je suis sur le model S$")
+    public void je_suis_sur_le_model_S() throws Throwable {
+        driver.get("https://www.tesla.com/fr_fr/models/design");
     }
 
-    @Then("^la deuxieme punchline contient \"([^\"]*)\"$")
-    public void la_deuxieme_punchline_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//*[contains(text(),'Model 3')]")).getAttribute("innerHTML").contains(arg1));
+    @Then("^Le prix affiché  est de \"([^\"]*)\"$")
+    public void le_prix_affiché_est_de(String arg1) throws Throwable {
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/div/div[2]/div[2]/p")).getText(),arg1);
+
     }
 
-    @Then("^la troisieme punchline contient \"([^\"]*)\"$")
-    public void la_troisieme_punchline_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//*[contains(text(),'Model X')]")).getAttribute("innerHTML").contains(arg1));
+    @Then("^Quand je click sur \"([^\"]*)\"$")
+    public void quand_je_click_sur(String arg1) throws Throwable {
+        driver.findElement(By.cssSelector(".finance-content--modal")).click();
     }
 
-    @Then("^la quatrieme punchline contient \"([^\"]*)\"$")
-    public void la_quatrieme_punchline_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//*[contains(text(),'Model Y')]")).getAttribute("innerHTML").contains(arg1));
+    @Then("^Le prix du montant total avec achat au terme du contrat de \"([^\"]*)\"$")
+    public void le_prix_du_montant_total_avec_achat_au_terme_du_contrat_de(String arg1) throws Throwable {
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"totalLeaseAmount\"]")).getAttribute("value"),arg1);
     }
 
-    @Then("^la cinquieme punchline contient \"([^\"]*)\"$")
-    public void la_cinquieme_punchline_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElements(By.cssSelector("h1.tcl-hero-parallax__heading-color-black")).get(5).getAttribute("innerHTML").contains(arg1));
+    @Given("^je suis sur commander Model S$")
+    public void je_suis_sur_commander_Model_S() throws Throwable {
+        driver.get("https://www.tesla.com/fr_fr/models/design");
     }
 
-    @Then("^les liens du menus contiennent \"([^\"]*)\" et \"([^\"]*)\"$")
-    public void les_liens_du_menus_contiennent_et(String arg1, String arg2) throws Throwable {
-        assertEquals(driver.findElement(By.xpath("//a[contains(text(), '"+arg1+"' )]")).getAttribute("href"),arg2);
+    @When("^je click sur Performance \"([^\"]*)\"$")
+    public void je_click_sur_Performance(String arg1) throws Throwable {
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/div[2]/div/div[3]/div[1]/div[3]/div[2]")).click();
+        Thread.sleep(10000);
     }
 
-    @Then("^le premier lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_premier_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Véhicules ')]")).getAttribute("innerHTML").contains(arg1));
+    @Then("^le prix affiché en LOA PERFORMANCE est de \"([^\"]*)\"$")
+    public void le_prix_affiché_en_LOA_PERFORMANCE_est_de(String arg1) throws Throwable {
+        Thread.sleep(10000);
+        assertEquals(driver.findElement(By.cssSelector(".finance-item--price.finance-item--price-before-savings")).getText(),arg1);
     }
 
-    @Then("^le deuxieme lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_deuxieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElements(By.xpath("//a[contains(text(),'Véhicules ')]")).get(3).getAttribute("innerHTML").contains(arg1));
+    @Then("^Le prix affiché est \"([^\"]*)\"$")
+    public void le_prix_affiché_est(String arg1) throws Throwable {
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[3]/div/div/div/div[2]/div[2]/p")).getText(),arg1);
+
     }
 
-    @Then("^le troisieme lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_troisieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Reprise')]")).getAttribute("innerHTML").contains(arg1));
+    @Then("^Quand je click sur le bouton \"([^\"]*)\"$")
+    public void quand_je_click_sur_le_bouton(String arg1) throws Throwable {
+        driver.findElement(By.cssSelector(".finance-content--modal")).click();
     }
 
-    @Then("^le quatrieme lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_quatrieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Cybertruck')]")).getAttribute("innerHTML").contains(arg1));
+    @Then("^Le prix du montant total avec achat au terme du contrat est alors de \"([^\"]*)\"$")
+    public void le_prix_du_montant_total_avec_achat_au_terme_du_contrat_est_alors_de(String arg1) throws Throwable {
+        assertEquals(driver.findElement(By.xpath("//*[@id=\"totalLeaseAmount\"]")).getAttribute("value"),arg1);
     }
 
-    @Then("^le cinquieme lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_cinquieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Roadster')]")).getAttribute("innerHTML").contains(arg1));
+    @Given("^je suis sur commander ModelS$")
+    public void je_suis_sur_commander_ModelS() throws Throwable {
+        driver.get("https://www.tesla.com/fr_fr/models/design");
     }
 
-    @Then("^le sixieme lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_sixieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//a[contains(text(), 'Énergie')]")).getAttribute("innerHTML").contains(arg1));
+    @When("^je click sur \"([^\"]*)\"$")
+    public void je_click_sur(String arg1) throws Throwable {
+
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[1]/div[2]/ul/li[4]")).click();
     }
 
-    @Then("^le septieme lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_septieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Essais')]")).getAttribute("innerHTML").contains(arg1));
+    @Then("^je click sur l'option \"([^\"]*)\"$")
+    public void je_click_sur_l_option(String arg1) throws Throwable {
+
+        driver.findElement(By.xpath("/html/body/div[1]/div/main/div/div/div[2]/div[2]/div/div/div/div[2]/div[2]/div[2]/div[1]")).click();
     }
 
-    @Then("^le huitieme lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_huitieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        String[] tab = arg1.split(" ");
-        WebElement tmp = driver.findElement(By.xpath("//a[contains(text(),'Flottes ')]"));
-        assertTrue(tmp.getAttribute("innerHTML").contains(tab[0]));
-        assertTrue(tmp.getAttribute("innerHTML").contains(tab[2]));
+    @Then("^le prix affiché en LOA est alors de \"([^\"]*)\"$")
+    public void le_prix_affiché_en_LOA_est_alors_de(String arg1) throws Throwable {
+
+        assertEquals(driver.findElement(By.cssSelector(".finance-item--price.finance-item--price-before-savings")).getText(),arg1);
     }
 
-    @Then("^le neuvieme lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_neuvieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Nous trouver')]")).getAttribute("innerHTML").contains(arg1));
+    @Given("^je suis sur la page  model S$")
+    public void je_suis_sur_la_page_model_S() throws Throwable {
+        driver.get("https://www.tesla.com/fr_fr/models/");
     }
 
-    @Then("^le dixieme lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_dixieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Événements')]")).getAttribute("innerHTML").contains(arg1));
+    @Then("^je configure mon modèle S$")
+    public void je_configure_mon_modèle_S() throws Throwable {
+        driver.findElement(By.cssSelector("a[href='https://www.tesla.com/fr_fr/']")).click();
+        Thread.sleep(3000);
     }
 
-    @Then("^le onzieme lien contient burger menu contient \"([^\"]*)\"$")
-    public void le_onzieme_lien_contient_burger_menu_contient(String arg1) throws Throwable {
-        assertTrue(driver.findElement(By.xpath("//a[contains(text(),'Assistance')]")).getAttribute("innerHTML").contains(arg1));
+    @Then("^je clic donc sur le logo en haut à gauche de la page puis en bas de page sur le lien Localisations j'arrive sur la page dont l'url est : \"([^\"]*)\"$")
+    public void je_clic_donc_sur_le_logo_en_haut_à_gauche_de_la_page_puis_en_bas_de_page_sur_le_lien_Localisations_j_arrive_sur_la_page_dont_l_url_est(String arg1) throws Throwable {
+        driver.findElement(By.cssSelector("a.tds-menu-header-main--logo")).click();
+        Thread.sleep(3000);
+        assertEquals(driver.getCurrentUrl(), "https://www.tesla.com/fr_fr/");
+
+        driver.findElement(By.tagName("body")).click();
+        driver.findElement(By.tagName("body")).sendKeys(Keys.END);
+        Thread.sleep(3000);
+
+        driver.findElement(By.cssSelector("a[href='/fr_fr/findus/list']")).click();
+        System.out.println(driver.getCurrentUrl());
+
     }
 
     @After
